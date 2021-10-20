191a78de0edb76e745b1db026e49987b0f556dd9
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-10-20 01:13:02 -0300

    att1

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { App } from './App'
+import { AuthProvinder } from './contexts/auth'
+import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvinder>
+      <App />
+    </AuthProvinder>
   </React.StrictMode>,
   document.getElementById('root')
 )
